3f0bcc9a347e468a2c26f53ba188858253c35c08
vladvladislavsky <vladvladislavsky@gmil.com>
2018-01-20 20:04:51 +0300

    commit-test-2

--- header.php
+++ header.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <div>
-<title>New-f 22222222</title>
-<title>Master 1111111</title>
-<!--    master end-->
+    <title>New-f 22222222</title>
+    <title>Master 1111111</title>
+    <!--    master end-->
     <p>iEto dobavli v new-f</p>
     <p>iEto dobavli v new-f</p>
     <p>iEto dobavli v new-f</p>
-<!--    master merge 2-->
+    <p>iEto dobavli v new-f</p>
+    <p>iEto dobavli v new-f</p>
+    <!--    master merge 2-->
 </div>
