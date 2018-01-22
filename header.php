375fb00f53be3beafb9fb97fe26bcde4c91007f9
vladvladislavsky <vladvladislavsky@gmil.com>
2018-01-22 15:35:41 +0300

    TEST0

--- header.php
+++ header.php
@@ -1,28 +1,4 @@
 <!DOCTYPE html>
 <div>
-    <title>New-f 22222222</title>
-    <title>Master 1111111</title>
-    <!--    master end-->
-    <p>iEto dobavli v new-f</p>
-    <p>iEto dobavli v new-f</p>
-    <p>iEto dobavli v new-f</p>
-    <p>iEto dobavli v new-f</p>
-    <p>iEto dobavli v new-f</p>
-    <p>iEto dobavli v new-f</p>
-    <p>iEto dobavli v new-f</p>
-    <p>iEto dobavli v new-f</p>
-    <p>iEto dobavli v new-f</p>
-    <p>iEto dobavli v new-f</p>
-    <p>iEto dobavli v new-f</p>
-    <p>iEto dobavli v new-f</p>
-    <p>iEto dobavli v new-f</p>
-    <p>iEto dobavli v new-f</p>
-    <p>iEto dobavli v new-f</p>
-    <p>iEto dobavli v new-f</p>
-    <p>iEto dobavli v new-f</p>
-    <p>iEto dobavli v new-f</p>
-    <p>iEto dobavli v new-f</p>
-    <p>iEto dobavli v new-f</p>
-    <p>iEto dobavli v new-f</p>
-    <!--    master merge 2-->
+    <title>TEST0 TEST0 TEST0 TEST0 TEST0 TEST0 TEST0 TEST0 TEST0 TEST0 TEST0 TEST0 TEST0 TEST0 TEST0 TEST0 TEST0 </title>
 </div>
